6f04a00f2f004c3d10ffeb23644a20f8561c35d6
Aryan Goyal <you@example.com>
2021-07-01 23:18:52 +0530

    First Commit

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+
+import '../widgets/order_item.dart';
+import '../providers/orders.dart' show Orders;
+
+class OrdersScreen extends StatefulWidget {
+  static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Building orders');
+    // final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Order'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: _ordersFuture,
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              //do error handling
+              return Center(
+                child: Text('An error Occured!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
